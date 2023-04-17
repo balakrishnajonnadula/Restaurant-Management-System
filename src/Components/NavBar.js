f95d33098ba23faa2796e00c5053e04c92b38bcb
balakrishnajonnadula <myself.jbk@gmail.com>
2023-04-17 12:51:51 +0530

    Login ui completed

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "../styles/navbar.css";
-import title from "../assets/title.jpg";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   useEffect(() => {
