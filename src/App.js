abe2aa2bfe31843b2d76a2ceefa8c3ef3af1207a
pagadalakalyanojas <kalyan.pagadala@ojas-it.com>
2023-04-17 12:41:49 +0530

    categories added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
+import Home from "./Components/Home";
+
+import Login from "./Components/Login";
+import MainPage from "./Components/MainPage";
+import Categories from "./Components/Categories";
 
 function App() {
   return (
     <div className="App">
-     Hello jbk  
-     Hi Kalyan
-     Hi Team
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />}>
+            <Route path="/" exact element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/categories" element={<Categories />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
