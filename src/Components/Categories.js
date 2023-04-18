3c4597728d2879ef140cbf012ecf98ed474d8f31
pagadalakalyanojas <kalyan.pagadala@ojas-it.com>
2023-04-18 10:15:37 +0530

    categories changes

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import categoriesService from '../services/categoriesService';
-
+import categories from '../styles/categories.css';
 
 const Categories = () => {
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        categoriesService.getCategories(res=> setCategories(res));
+        categoriesService.getCategories(res => setCategories(res));
     }, []);
 
     console.log(categories);
@@ -15,12 +15,12 @@ const Categories = () => {
             <div Name='row' >
                 {
                     categories.map((item, index) =>
-                        <div Name='col-lg-3 col-md-4 col-sm-12' key={index}>
-                            <div Name='m-5'>
+                        <div Name='col-lg-3 col-md-4 col-sm-12 categories' key={index}>
+                            <div Name='mt-5'>
                                 <div Name='mb-3'>
                                     <img src={item.image} alt={item.slug} height={"200px"} width={"200px"} />
                                 </div>
-                                <div Name=''>
+                                <div Name='mb-2'>
                                     <h4 Name='text-center'>{item.name}</h4>
                                 </div>
                             </div>
