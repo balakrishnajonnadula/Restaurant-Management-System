4027cb4eb819dea0c4fae12c5685881cd4c89bd9
balakrishnajonnadula <myself.jbk@gmail.com>
2023-04-27 11:03:39 +0530

    changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,24 +2,36 @@ import React, { useState } from "react";
 import loginImg from "../assets/brokeplate.jpg";
 import "../styles/Login.css";
 import loginService from "../services/loginService";
-
+import axios from "axios";
 
 const Login = () => {
   const [uName, setUname] = useState("");
   const [password, setPassword] = useState("");
+  const [response, setResponse] = useState();
+  const[tok,setTok] = useState();
 
-<<<<<<< HEAD
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log(" login details : " + uName + " " + password);
+
     let user = {
       username: uName,
       password: password,
     };
-    loginService.login(user).then(res=> console.log(res.data)).catch(err=> alert(err));
+
+    // loginService.login(user).then((res) => {
+    //   setResponse(res.data);
+    //   setTok(res.data.token);
+    // });
+    // localStorage.setItem("token",response.token);
+    // if (response.token) {
+    //   console.log("token : ", response.token);
+    // }
+    axios.post("http://192.168.7.221:8000/login/",user)
+    .then(resp => {
+      console.log("resp ",resp.data.token);
+    })
+    
   };
-=======
->>>>>>> origin/master
 
   return (
     <div className="container-fluid">
@@ -28,15 +40,11 @@ const Login = () => {
           className="col-lg-8 col-md-7  d-none d-lg-block"
           style={{ padding: "0px" }}
         >
-<<<<<<< HEAD
           <img
             src={loginImg}
             alt="bg"
             style={{ height: "100vh", width: "100%" }}
           />
-=======
-          <img src={loginImg} alt="bg" style={{ height: "100vh", width: "100%" }} />
->>>>>>> origin/master
         </div>
         <div
           className="col-lg-4 col-md-5 col-md-12 px-5"
@@ -56,17 +64,10 @@ const Login = () => {
                   className="custom-input"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
-<<<<<<< HEAD
                   onChange={(e) => {
                     setUname(e.target.value);
                   }}
                 />
-=======
-                  onChange={(e) => { setUname(e.target.value) }}
-
-                />
-
->>>>>>> origin/master
               </div>
               <div className="mb-3">
                 <label className="form-label">Password</label>
@@ -74,34 +75,27 @@ const Login = () => {
                   type="password"
                   className="custom-input"
                   id="exampleInputPassword1"
-<<<<<<< HEAD
                   onChange={(e) => {
                     setPassword(e.target.value);
                   }}
                 />
               </div>
 
-              <div className="d-flex justify-content-center my-4 ">
+              <div className="d-flex justify-content-center my-4">
                 <button
                   className="custom-btn py-3 w-100"
+                  style={{ backgroundColor: "black" }}
                   onClick={(e) => {
                     handleLogin(e);
                   }}
                 >
                   Login
                 </button>
-=======
-                  onChange={(e) => { setPassword(e.target.value) }}
-                />
-              </div>
-
-              <div class="d-flex justify-content-center my-4">
-                <button className="custom-btn py-3 w-100" style={{backgroundColor:'black'}}>Login</button>
-
               </div>
               <div>
-                <a href="/signup" style={{ textDecoration: 'none' }}>Create an Account</a>
->>>>>>> origin/master
+                <a href="/signup" style={{ textDecoration: "none" }}>
+                  Create an Account
+                </a>
               </div>
             </form>
           </div>
